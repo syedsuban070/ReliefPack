eeb70d1f4e38f78e032ae7f1f52721cd45a82962
EvidenceCFO <syedsuban070@gmail.com>
2026-09-25 12:58:29 +0500

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'reliefpack-v1';
+const ASSETS = ['./', './index.html', './style.css', './app.js', './manifest.webmanifest'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('reliefpack-') && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
